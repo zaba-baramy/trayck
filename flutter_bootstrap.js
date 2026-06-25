0b2b4299d4c869deb1e198fba750a7fe82eb5a05
zaba-baramy <zaba-baramy@users.noreply.github.com>
2026-06-25 17:53:55 +0000

    deploy: 17a8bf6b0bb289ffa84a6c72d8c1de13a36eeae4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "556060734" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1025818480" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
